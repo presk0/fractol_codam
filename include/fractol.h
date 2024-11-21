09645bc8b02f2847fb056c7137821c8b474a4c60
Nicolas Dionis <nidionis@k1r2p6.42mulhouse.fr>
2024-11-21 20:32:13 +0100

    mlx_SET_WINDOW_LIMIT AVOID SEGFAULT

--- include/fractol.h
+++ include/fractol.h
@@ -6,7 +6,7 @@
 /*   By: supersko <supersko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 18:19:05 by supersko          #+#    #+#             */
-/*   Updated: 2024/11/21 13:37:59 by nidionis         ###   ########.fr       */
+/*   Updated: 2024/11/21 17:02:35 by nidionis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,30 @@
 # include <stdint.h>
 # include <libft.h>
 # include <MLX42/MLX42.h>
+
+# define ARROW_RIGHT 262
+# define ARROW_LEFT 263
+# define ARROW_DOWN 264
+# define ARROW_UP 265
+# define KEY_Q 81
+# define KEY_A 65
+# define KEY_W 87
+# define KEY_S 83
+# define KEY_E 69
+# define KEY_D 68
+
 # define ZOOM_STEP 0.1
 # define WIDTH 1024
-# define HEIGHT 1024
-# define DEFAULT_ITER_MAX 256
+# define HEIGHT 512
+# define DEFAULT_ITER_MAX 42
 # define GREEN get_rgba(0, 255, 0, 255)
 # define BLUE get_rgba(0, 0, 255, 255)
 # define RED get_rgba(255, 0, 0, 255)
 # define BPP sizeof(int32_t)
 
-/*
-typedef struct mlx
-{
-	void*		window;
-	void*		context;
-	int32_t		width;
-	int32_t		height;
-	double		delta_time;
-}	mlx_t;
-*/
+typedef mlx_t t_mlx;
+
+typedef struct s_parami s_param;
 
 typedef struct s_pix
 {
@@ -56,7 +61,13 @@ typedef struct s_complex
 	double	i;
 }	t_complex;
 
-typedef mlx_t t_mlx;
+
+typedef struct s_data
+{
+	t_mlx				*mlx;
+	mlx_image_t			*img;
+	struct s_param		*param;
+}	t_data;
 
 typedef struct s_param 
 {
@@ -64,13 +75,7 @@ typedef struct s_param
 	t_complex	julia_coef;
 	double		zoom;
 	t_pix		offset;
+	void		(*f)(t_data *d, t_pix pix);
 }	t_param;
-
-typedef struct s_data
-{
-	t_mlx		*mlx;
-	mlx_image_t	*img;
-	t_param		param;
-}	t_data;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,11 +6,13 @@
 /*   By: supersko <supersko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 18:24:29 by supersko          #+#    #+#             */
-/*   Updated: 2024/11/21 13:50:38 by nidionis         ###   ########.fr       */
+/*   Updated: 2024/11/21 20:30:59 by nidionis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fractol.h>
+
+int	loops(t_data *data);
 
 int get_rgba(int r, int g, int b, int a)
 {
@@ -70,25 +72,6 @@ t_complex	complex_sum(t_complex z1, t_complex z2)
 	return (sum);
 }
 
-static void ft_hook(void* param)
-{
-	const t_mlx* mlx = param;
-	(void)mlx;
-
-	//printf("WIDTH: %d | HEIGHT: %d\n", mlx->width, mlx->height);
-}
-/*
-void	orthonormal(t_data *data, t_pix *i, int color)
-{
-	int	black;
-
-	black = get_rgba(0, 0, 0, 0);
-	if (i->x == HEIGHT / 2 || i->y == WIDTH / 2)
-		mlx_put_pixel(data->img, i->y, i->x, color);
-	else
-		mlx_put_pixel(data->img, i->y, i->x, black);
-}
-*/
 unsigned	complex_square_module(t_complex p)
 {
 	return (p.r * p.r + p.i * p.i);
@@ -102,8 +85,8 @@ int	julia_iter(t_data *data, t_complex p)
 	t_complex	coef;
 
 	iter = 0;
-	coef = data->param.julia_coef;
-	iter_max = data->param.iter_max;
+	coef = data->param->julia_coef;
+	iter_max = data->param->iter_max;
     while (complex_square_module(p) <= 4 && iter < iter_max)
 	{
         tmp = p.r * p.r - p.i * p.i + coef.r;
@@ -120,16 +103,18 @@ t_complex	pixel_to_complex(t_data *data, t_pix p)
 	double		numerator;
 	double		denominator;
 
-	numerator = p.x - (double)WIDTH / 2.0;
-	denominator = data->param.zoom * (double)WIDTH / 2.0;
-    z.r = numerator / denominator + data->param.offset.x;
-	numerator = p.y - (double)HEIGHT / 2.0;
-	denominator = data->param.zoom * (double)HEIGHT / 2.0;
-    z.i = numerator / denominator + data->param.offset.y;
+	numerator = (double)p.x - (double)data->img->width / 2.0;
+	denominator = data->param->zoom * (double)data->img->width / 2.0;
+	if (denominator)
+		z.r = numerator / denominator + data->param->offset.x;
+	numerator = (double)p.y - (double)data->img->height / 2.0;
+	denominator = data->param->zoom * (double)data->img->height / 2.0;
+	if (denominator)
+		z.i = numerator / denominator + data->param->offset.y;
 	return (z);
 }
 
-int get_color(int iter) {
+int julia_multicolor(int iter) {
     if (iter == DEFAULT_ITER_MAX)
         return 0x000000;
     double t = (double)iter / DEFAULT_ITER_MAX;
@@ -140,29 +125,31 @@ int get_color(int iter) {
 }
 
 void draw_pixel(t_data *data, t_pix pix, int color) {
-    mlx_put_pixel(data->img, pix.y, pix.x, color);
+    if (!data || !data->img || !data->mlx) {
+        fprintf(stderr, "Error: Invalid data or uninitialized structures\n");
+        return;
+    }
+
+    if (pix.x >= 0 && pix.x < (int)data->mlx->width &&
+        pix.y >= 0 && pix.y < (int)data->mlx->height) {
+        mlx_put_pixel(data->img, pix.x, pix.y, color);
+    } else {
+        fprintf(stderr, "Warning: Out-of-bounds pixel (%d, %d)\n", pix.x, pix.y);
+    }
 }
+
+/*
+void draw_pixel(t_data *data, t_pix pix, int color) {
+	if (pix.y < (int)data->mlx->height && pix.x < (int)data->mlx->width)
+		mlx_put_pixel(data->img, pix.x, pix.y, color);
+}
+*/
 
 void render_julia(t_data *data, t_pix pix) {
 	t_complex c = pixel_to_complex(data, pix);
 	int iter = julia_iter(data, c);
-	int color = get_color(iter);
+	int color = julia_multicolor(iter);
 	draw_pixel(data, pix, color);
-}
-
-void	julia(t_data *data, t_pix *i, int color)
-{
-	int			count;
-	int			iter_max;
-	int			module;
-	t_complex	i_nb;
-	(void)color;
-
-	iter_max = data->param.iter_max;
-	count = 0;
-	i_nb = pixel_to_complex(data, *i);
-	module = julia_iter(data, i_nb);
-	mlx_put_pixel(data->img, i->y, i->x, module);
 }
 
 void	prepare_next_frame(t_data *data, void (*f)(t_data *data, t_pix i))
@@ -170,10 +157,10 @@ void	prepare_next_frame(t_data *data, void (*f)(t_data *data, t_pix i))
 	t_pix	i;
 	
 	set_pix(&i, 0, 0);
-	while (i.x < WIDTH)
+	while (i.x < (int)data->mlx->width)
 	{
 		i.y = 0;
-		while (i.y < HEIGHT)
+		while (i.y < (int)data->mlx->height)
 		{
 			f(data, i);
 			(i.y)++;
@@ -184,11 +171,13 @@ void	prepare_next_frame(t_data *data, void (*f)(t_data *data, t_pix i))
 
 void	init_data(t_data *data)
 {
-	set_complex(&data->param.julia_coef, -0.7, 0.27015);
-	data->param.iter_max = DEFAULT_ITER_MAX;
-	data->param.zoom = 1;
-	data->param.offset.x = 0;
-	data->param.offset.y = 0;
+	data->param = malloc(sizeof(t_param));
+	set_complex(&data->param->julia_coef, -0.7, 0.27015);
+	data->param->iter_max = DEFAULT_ITER_MAX;
+	data->param->zoom = 1;
+	data->param->offset.x = 0;
+	data->param->offset.y = 0;
+	mlx_set_window_limit(data->mlx, 1, 1, WIDTH, HEIGHT);
 }
 
 void my_scrollhook(double xdelta, double ydelta, void *data)
@@ -198,13 +187,60 @@ void my_scrollhook(double xdelta, double ydelta, void *data)
 
 	d = data;
 	(void)xdelta;
-	p = &(((t_data *)data)->param);
+	p = ((t_data *)data)->param;
 	if (ydelta > 0)
-		p->zoom += ZOOM_STEP;
+	{
+		if ((p->zoom + ZOOM_STEP) * p->zoom > 0)
+			p->zoom += ZOOM_STEP;
+	}
 	else if (ydelta < 0)
-		p->zoom -= ZOOM_STEP;
-	prepare_next_frame(data, render_julia);
-	mlx_image_to_window(d->mlx, d->img, 0, 0);
+	{
+		if ((p->zoom - ZOOM_STEP) * p->zoom > 0)
+			p->zoom -= ZOOM_STEP;
+	}
+	loops(data);
+}
+
+void resize_win(int width, int height, void *data)
+{
+	(void)width;
+	(void)height;
+	t_data	*d;
+
+	d = (t_data *)data;
+	printf("mlx->width= %i\n", d->mlx->width);
+	printf("mlx->height= %i\n", d->img->height);
+	printf("img->width= %i\n", d->mlx->width);
+	printf("img->height= %i\n", d->mlx->height);
+	//d->img->width = width;
+	//d->img->height = height;
+	loops(data);
+}
+
+void	my_keyhook(mlx_key_data_t keydata, void *data)
+{
+	(void)data;
+	(void)keydata;
+	//printf("keydata.key = %i\n", keydata.key);
+	//printf("keydata.action = %i\n", keydata.action);
+	//printf("keydata.os_key = %i\n", keydata.os_key);
+	//printf("keydata.modifier = %i\n", keydata.modifier);
+}
+
+int	loops(t_data *data)
+{
+	prepare_next_frame(data, data->param->f);
+	if (mlx_image_to_window(data->mlx, data->img, 0, 0) == -1)
+	{
+		mlx_close_window(data->mlx);
+		puts(mlx_strerror(mlx_errno));
+		return(EXIT_FAILURE);
+	}
+	mlx_scroll_hook(data->mlx, &my_scrollhook, data);
+	mlx_key_hook(data->mlx, &my_keyhook, data);
+	mlx_resize_hook(data->mlx, &resize_win, data);
+	mlx_loop(data->mlx);
+	return (0);
 }
 
 int	main(int argc, char *argv[])
@@ -220,26 +256,25 @@ int	main(int argc, char *argv[])
 		help_msg();
 	else
 	{
+		//mlx_set_setting(MLX_MAXIMIZED, true);
 		data.mlx = mlx_init(WIDTH, HEIGHT, "fractol", true);
 		if (!data.mlx)
 		{
-			ft_errmsg("mlx_init crashed\n");
-			return (-1);
+			ft_errmsg((char *)mlx_strerror(mlx_errno));
+			return (EXIT_FAILURE);
 		}
 		init_data(&data);
+		data.param->f = &render_julia;
 		data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
 		if (!data.img || (mlx_image_to_window(data.mlx, data.img, 0, 0) < 0))
 		{
-			ft_errmsg("mlx_new_image crashed\n");
-			mlx_terminate(data.mlx);
-			return (-1);
+			mlx_close_window(data.mlx);
+			ft_errmsg((char *)mlx_strerror(mlx_errno));
+			return(EXIT_FAILURE);
 		}
-		prepare_next_frame(&data, render_julia);
-		mlx_image_to_window(data.mlx, data.img, 0, 0);
-		mlx_loop_hook(data.mlx, ft_hook, data.mlx);
-		mlx_scroll_hook(data.mlx, &my_scrollhook, &data);
-		mlx_loop(data.mlx);
-		mlx_terminate(data.mlx);
+		if (loops(&data) == EXIT_FAILURE)
+			return (EXIT_FAILURE);
 	}
+	free(data.param);
 	return (0);
 }
